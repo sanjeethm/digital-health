c28d316af9982448571bf70f8552fa0ba6e79f58
sanjeethm <sanjeethm@gmail.com>
2024-07-29 01:05:18 +0530

    added few files

--- src/pages/Solutions.js
+++ src/pages/Solutions.js
@@ -1,22 +1,34 @@
 import React from 'react';
+import { Container, Row, Col, Card } from 'react-bootstrap';
+import './Solutions.css';
 
 const Solutions = () => {
   return (
-    <div className="container">
+    <Container className="solutions-section">
       <h1>Our Solutions</h1>
-      <p>Placeholder for solutions overview...</p>
+      <p className="lead">We offer a range of solutions to meet your business needs...</p>
       
-      <div className="row">
-        <div className="col-md-6">
-          <h2>Solution 1</h2>
-          <p>Details about Solution 1...</p>
-        </div>
-        <div className="col-md-6">
-          <h2>Solution 2</h2>
-          <p>Details about Solution 2...</p>
-        </div>
-      </div>
-    </div>
+      <Row>
+        <Col md={6}>
+          <Card className="solution-card">
+            <Card.Body>
+              <Card.Title>Solution 1</Card.Title>
+              <Card.Text>Details about Solution 1...</Card.Text>
+            </Card.Body>
+            <Card.Img variant="bottom" src="/solution1.svg" />
+          </Card>
+        </Col>
+        <Col md={6}>
+          <Card className="solution-card">
+            <Card.Body>
+              <Card.Title>Solution 2</Card.Title>
+              <Card.Text>Details about Solution 2...</Card.Text>
+            </Card.Body>
+            <Card.Img variant="bottom" src="/solution2.svg" />
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
